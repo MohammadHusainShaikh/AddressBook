96f232669506cf20f4f6ee8b12a5c3b4cf3f05fc
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-05-11 01:20:51 +0530

    [MODIFIED]:Husain - UC 5 Modified for multiple edit and delete

--- src/com/self/addressbook/AddressBook.java
+++ src/com/self/addressbook/AddressBook.java
@@ -29,66 +29,81 @@ public class AddressBook {
 	}
 	
 	public void displayContacts() {
-		for(int i = 0; i<contactList.size(); i++) {
-			Contact contact = contactList.get(i);
-			System.out.println(contact.toString());
+		if(!contactList.isEmpty()) {
+			for(int i = 0; i<contactList.size(); i++) {
+				Contact contact = contactList.get(i);
+				System.out.println(contact.toString());
+			}
 		}
 	}
 	
-/*	public void editContact() {
-		
-		String fname;
-		System.out.println("Enter the First name to edit contact");
-		fname = scanner.next();
-		
-		if(fname.equals(contact.getFirstName()))
-		{
-			
-			System.out.println("Contact list found");
-			System.out.println("Enter the last name");
-			contact.setLastName(scanner.next());
-			System.out.println("Enter the address");
-			contact.setAddress(scanner.next());
-			System.out.println("Enter the city name");
-			contact.setCity(scanner.next());
-			System.out.println("Enter the state name");
-			contact.setState(scanner.next());
-			System.out.println("Enter the zip code");
-			contact.setZip(scanner.next());
-			System.out.println("Enter the Phone number");
-			contact.setPhoneNo(scanner.next());
-			System.out.println("Enter the email id");
-			contact.setEmailId(scanner.next());
-		}
-			else {
-			System.out.println("Contact list not found");
-		}
-	}
-	
-	public void deleteContact() {
-		
-		String fname;
-		System.out.println("Enter the first name to delete a contact");
-		fname = scanner.next();
-		if(fname.equals(contact.getFirstName()))
-			{
-				System.out.println("Contact Deleted");
-				contact = null;
-			}else
+	public void editContacts() {
+		if(!contactList.isEmpty()){
+			String fname;
+			System.out.println("Enter the First name to edit contact");
+			fname = scanner.next();
+			Contact contact = null;
+			boolean flag = false;
+			for(int i = 0; i<contactList.size(); i++) {
+				contact = contactList.get(i);
+				if(fname.equals(contact.getFirstName()))
 				{
-				System.out.print("name not found");
+					flag = true;
+					System.out.println("Contact list found");
+					System.out.println("Enter the last name");
+					contact.setLastName(scanner.next());
+					System.out.println("Enter the address");
+					contact.setAddress(scanner.next());
+					System.out.println("Enter the city name");
+					contact.setCity(scanner.next());
+					System.out.println("Enter the state name");
+					contact.setState(scanner.next());
+					System.out.println("Enter the zip code");
+					contact.setZip(scanner.next());
+					System.out.println("Enter the Phone number");
+					contact.setPhoneNo(scanner.next());
+					System.out.println("Enter the email id");
+					contact.setEmailId(scanner.next());
 				}
-	}*/
+				else
+				{
+					System.out.println("Contact not found");
+				}
+			}
+		}
+	}
+	public void deleteContact() {
+		if(!contactList.isEmpty()) {
+			String fname;
+			System.out.println("Enter the first name to delete a contact");
+			fname = scanner.next();
+			boolean flag = false;
+			for(int i = 0; i<contactList.size(); i++) {
+				
+				Contact contact = contactList.get(i);
+				if(fname.equals(contact.getFirstName()))
+					{
+						flag = true;
+						contactList.remove(contact);
+						System.out.println("Contact is deleted");	
+					}
+				else
+					{
+						System.out.println("Contact not found");
+					}
+			}		
+		}
+	}
 		public static void main(String[] args) {
 		AddressBook addressBook = new AddressBook();
 		int choice=0;
 		do {
 		System.out.println("***********Address Book***********");
 		System.out.println("1: Add");
-		//System.out.println("2: Edit");
-		//System.out.println("3: Delete");
-		System.out.println("2: Display");
-		System.out.println("3: Exit");
+		System.out.println("2: Edit");
+		System.out.println("3: Delete");
+		System.out.println("4: Display");
+		System.out.println("5: Exit");
 		System.out.println("Enter your choice");
 		choice = addressBook.scanner.nextInt();
 			switch(choice) {
@@ -97,14 +112,22 @@ public class AddressBook {
 				addressBook.addContact();
 			break;
 			case 2 :
+				System.out.println("Edit contact");
+				addressBook.editContacts();
+			break;
+			case 3 :
+				System.out.println("Delete contact");
+				addressBook.deleteContact();
+			break;
+			case 4 :
 				System.out.println("Display contact");
 				addressBook.displayContacts();
 			break;
-			case 3:
+			case 5:
 				System.out.println("Exit");
 			break;
 			}
-		}while(choice<=3);
+		}while(choice<=4);
 	}
 
 }
